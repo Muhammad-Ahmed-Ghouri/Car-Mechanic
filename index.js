9b581868b5b0bbaf82a9b19a74d3e111f2ba7abe
Muhammad Ahmed <muhammadahmed7274@gmail.com>
2025-05-31 16:31:01 +0500

    made navbar responsive

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const navbar = document.getElementById("hamburger-navbar");
 let counter = 1;
+
 setInterval(() => {
   document.getElementById("radio" + counter).checked = true;
   counter++;
@@ -6,3 +8,11 @@ setInterval(() => {
     counter = 1;
   }
 }, 5000);
+
+function navbarVisibility() {
+  navbar.style.display = "flex";
+}
+
+function closeVisibility() {
+  navbar.style.display = "none";
+}
