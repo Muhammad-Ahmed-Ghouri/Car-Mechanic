8237f4244bc276ab029e3185406b9126e88fb88f
Muhammad Ahmed <muhammadahmed7274@gmail.com>
2025-07-02 11:09:04 +0500

    added carts count text at the top of cart summary

--- cart.js
+++ cart.js
@@ -2,6 +2,8 @@ import { navbarVisibility } from "./products.js";
 import { closeVisibility } from "./products.js";
 import { itemCount } from "./products.js";
 
+const itemsCountText = document.querySelector(".items-count-text");
+
 window.navbarVisibility = navbarVisibility;
 window.closeVisibility = closeVisibility;
 
@@ -10,5 +12,11 @@ const selectedProducts = JSON.parse(localStorage.getItem("cart"));
 localStorage.setItem("cart", JSON.stringify(selectedProducts));
 
 console.log(selectedProducts);
+
+function itemsCountTextUpdate(selectedProducts) {
+  itemsCountText.innerHTML = `<strong>${selectedProducts} item </strong> in your cart.`;
+}
+
+itemsCountTextUpdate(selectedProducts.length);
 
 itemCount(selectedProducts.length);
